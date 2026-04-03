647e0f6fee2f6a658bf84f565b7caf52575d8287
Yafet Abebe Feleke <yafetema15@gmail.com>
2026-04-03 14:31:07 +0300

    reformat

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -17,7 +17,6 @@ import { useAuth, useLanguage } from '../context';
 import { api } from '../utils/api';
 import { toast } from 'sonner';
 
-
 const steps = [
   { id: 'basic', label: 'Basic Info', icon: Calendar },
   { id: 'datetime', label: 'Date & Time', icon: Calendar },
